09b3e52c01d93432948d96a7cce066caac995067
Colin de Vries <colindevries325@gmail.com>
2020-02-02 05:41:52 -0800

    add nav toggle button

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const NavbarContainer = styled.div`
+  position: sticky;
+  top: 0;
+  left: 0;
   display: flex;
+  align-items: center;
+  height: 5.2rem;
+  padding: 0 1.6rem;
   color: white;
-  padding: 0.8rem 1.6rem;
   background: ${({ theme }) => theme.colors.primary};
   width: 100%;
 `;
@@ -15,10 +22,21 @@ const Name = styled.div`
   font-weight: 600;
 `;
 
+const NavToggleButton = styled.button`
+  margin-left: auto;
+  background: none;
+  border: none;
+  color: white;
+  font-size: 2.2rem;
+`;
+
 const Navbar = () => {
   return (
     <NavbarContainer>
       <Name>Colin de Vries</Name>
+      <NavToggleButton>
+        <FontAwesomeIcon icon={faBars} />
+      </NavToggleButton>
     </NavbarContainer>
   );
 };
