7a3c61b502ca54945fef5c77617fe2707def334d
Colin de Vries <colindevries325@gmail.com>
2020-02-04 05:29:58 -0800

    add content to education section

--- src/sections/education.js
+++ src/sections/education.js
@@ -1,15 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
+import css from '@styled-system/css';
 import SectionContainer from '../components/layout/section-container';
 import { H2, H3, H4 } from '../components/headings';
 import SectionDivider from '../components/section-divider';
+import { PSmall } from '../components/text';
+
+const EducationItemContainer = styled('div')(
+  css({
+    mb: 3
+  })
+);
+
+const EducationItemDescription = styled(PSmall)(
+  css({
+    mb: 2
+  })
+);
+
+const EducationItemDate = styled(PSmall)(
+  css({
+    color: 'primary',
+    fontWeight: 'medium'
+  })
+);
+
+const EducationItem = ({ heading, subheading, description, date }) => (
+  <EducationItemContainer>
+    <H3>{heading}</H3>
+    <H4>{subheading}</H4>
+    <EducationItemDescription>{description}</EducationItemDescription>
+    <EducationItemDate>{date}</EducationItemDate>
+  </EducationItemContainer>
+);
 
 const EducationSection = () => {
   return (
     <>
       <SectionContainer>
         <H2>Education</H2>
-        <H3>Lambda School</H3>
-        <H4>Full Stack Web Development</H4>
+        <EducationItem
+          heading='Lambda School'
+          subheading='Full Stack Web Development'
+          description='Full Stack Web Development Track'
+          date='May 2019 - March 2020'
+        />
+        <EducationItem
+          heading='Pioneer High School'
+          subheading='General Education'
+          description='GPA: 3.65'
+          date='August 2012 - June 2016'
+        />
       </SectionContainer>
       <SectionDivider />
     </>
