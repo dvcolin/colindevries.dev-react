387c82c5c81def434d5c819aa7d2ce8c50b94b93
Colin de Vries <colindevries325@gmail.com>
2020-02-05 07:09:50 -0800

    add custom favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Helmet } from 'react-helmet';
 import Layout from './components/layout/layout';
 import AboutSection from './sections/about';
 import ExperienceSection from './sections/experience';
@@ -11,19 +10,14 @@ import ProjectsSection from './sections/projects';
 const App = () => {
   const [visibleSection, setVisibleSection] = useState('about');
   return (
-    <>
-      <Helmet>
-        <title>Colin de Vries | Full Stack Web Developer</title>
-      </Helmet>
-      <Layout visibleSection={visibleSection}>
-        <AboutSection setVisibleSection={setVisibleSection} />
-        <ExperienceSection setVisibleSection={setVisibleSection} />
-        <EducationSection setVisibleSection={setVisibleSection} />
-        <SkillsSection setVisibleSection={setVisibleSection} />
-        <InterestsSection setVisibleSection={setVisibleSection} />
-        <ProjectsSection setVisibleSection={setVisibleSection} />
-      </Layout>
-    </>
+    <Layout visibleSection={visibleSection}>
+      <AboutSection setVisibleSection={setVisibleSection} />
+      <ExperienceSection setVisibleSection={setVisibleSection} />
+      <EducationSection setVisibleSection={setVisibleSection} />
+      <SkillsSection setVisibleSection={setVisibleSection} />
+      <InterestsSection setVisibleSection={setVisibleSection} />
+      <ProjectsSection setVisibleSection={setVisibleSection} />
+    </Layout>
   );
 };
 
